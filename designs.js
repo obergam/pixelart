317c016d607d97b780cf3ad031337475c9a67325
Ouaddah Bergam <obergam@yahoo.com>
2018-02-19 20:18:22 -0700

    just the first draft

--- designs.js
+++ designs.js
@@ -1,10 +1,23 @@
 // Select color input
+$("#submit").click(function (e) {
+    e.preventDefault();
+		gridHeight = $("#input-height").val();
+    gridWidth = $("#input-width").val();
+    grid.children().remove();
+    makeGrid();
+  });
 // Select size input
+let gridHeight = $('.inputHeight');
+let gridWidth = $('.inputWeight');
+
+const submit = document.getE ('submit');
+
+const canvas = document.getElementById('.pixelCanvas')
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+   
 }
+
+submit.click(makeGrid);
