414092c5722efa7950ef195123ce6ea2dcc48701
Ouaddah Bergam <obergam@yahoo.com>
2018-03-19 18:13:13 -0600

    Fixed preset functions. Site is all ready to go

--- designs.js
+++ designs.js
@@ -11,18 +11,24 @@ function makeGrid() {
     theGrid.append("<tr></tr>");
     for (c = 0; c < width.val(); c++) {
       theGrid
-        .children()
-        .last()
-        .append("<td></td>");
+        .children().last().append("<td></td>");
     }
   }
-}
-// default grid size
+};
 
+// default grid size
+$(document).ready(function() {
+  for (r = 0; r < 20; r++) {
+    theGrid.append("<tr></tr>");
+    for (c = 0; c < 20; c++) {
+      theGrid
+        .children().last().append("<td></td>");
+    }
+  }
+})
 
 // When user click submit make the Canvas
-
-$("#sizePicker").submit(function(event) {
+$("#submitButton").click(function(event) {
   theGrid.empty();
   event.preventDefault();
   makeGrid();
@@ -30,37 +36,35 @@ $("#sizePicker").submit(function(event) {
 
 //Clear the Canvas
 $("#clearBtn").click(function(){
+        event.preventDefault();
         $("td").css('background-color', '');
     });
 
-// event listener and background color change
+// Color the cells
 function colorTd (el) {
   let element = $(el.target);
   element.css('background-color', color.val());
-}
+};
 
+// Color on click
 theGrid.on("click", "td", function(event) {
   colorTd(event);
 });
 
-// event is added to table not cells
+// Creating variables for mouse clicks down and up
 let isDown = false;
 
 $(document).mousedown(function() {
-  // when mouse is down, isDown is true
   isDown = true;
 });
 
 $(document).mouseup(function() {
-  // when mouse is up, isDown is false
   isDown = false;
 });
 
-// event is added to table not cells
+// Drag function: color while mouse is down, stop when mouse is up
 $('#pixelCanvas').on("mousedown mousemove", function(event) {
-  // if event.target is a td, change color to input color
   if (event.target && event.target.nodeName == "TD") {
-    //when mouse is down
     if (isDown) {
       colorTd(event);
     }
@@ -69,21 +73,56 @@ $('#pixelCanvas').on("mousedown mousemove", function(event) {
 });
 
 
-// presets 10*10, 15*15, 20*20
-const preset1 = $('fifteen');
-const preset2 = $('twentyFive');
-const preset3 = $('thirtyFive');
+// PRESETS
 
-preset1.on("click", function () {
-  for (a = 0; a < 15; a++) {
+// 15*15
+const preset1 = $('#fifteen');
+
+preset1.click(function(event) {
+  theGrid.empty();
+  event.preventDefault();
+  for (r = 0; r < 15; r++) {
     theGrid.append("<tr></tr>");
-      for (b = 0; b < 15; b++) {
-        theGrid.children().last().append("<td></td>");
-      }
+    for (c = 0; c < 15; c++) {
+      theGrid
+        .children().last().append("<td></td>");
     }
-  });
+  }
+});
 
-// event listener for clearing cell with double click
+// 25*25
+const preset2 = $('#twentyFive');
+
+preset2.click(function(event) {
+  theGrid.empty();
+  event.preventDefault();
+  for (r = 0; r < 25; r++) {
+    theGrid.append("<tr></tr>");
+    for (c = 0; c < 25; c++) {
+      theGrid
+        .children().last().append("<td></td>");
+    }
+  }
+});
+
+// 35*35
+const preset3 = $('#thirtyFive');
+
+preset3.click(function(event) {
+  theGrid.empty();
+  event.preventDefault();
+  for (r = 0; r < 35; r++) {
+    theGrid.append("<tr></tr>");
+    for (c = 0; c < 35; c++) {
+      theGrid
+        .children().last().append("<td></td>");
+    }
+  }
+});
+
+
+
+// Delete coloring on double click
 theGrid.on("dblclick", "td", function() {
   $(this).css("background-color", "transparent");
 });
